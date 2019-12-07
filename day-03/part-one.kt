3d3d1387b9553c883dec0350542f00c0d55b1911
Juan de Bruin <juandb@pm.me>
2019-12-07 17:52:43 +0200

    Add line and point classes

---
+++ day-03/part-one.kt
@@ -0,0 +1,114 @@
+import java.io.File
+
+// I know it's dumb that I convert from instruction of form <direction><count>,
+// to line, then recalculate direction. I don't really want to do invest the
+// time to fix it right now.
+
+data class Point(val x: Int, val y: Int)
+
+data class Line(val a: Point, val b: Point) {
+	enum class Direction {
+		UP, DOWN, LEFT, RIGHT
+	}
+
+	enum class SimpleDirection {
+		VERTICAL, HORIZONTAL
+	}
+
+	private val direction =
+		if (a.x == b.x) {
+			if (a.y < b.y) Direction.RIGHT else Direction.LEFT
+		} else {
+			if (a.x < b.x) Direction.UP else Direction.DOWN
+		}
+
+	private val simpleDirection =
+		if (direction == Direction.UP || direction == Direction.DOWN) {
+			SimpleDirection.VERTICAL
+		} else {
+			SimpleDirection.HORIZONTAL
+		}
+
+	// Not sure if lines that run on top of each other are considered
+	// "intersecting" but I doubt the input will need me to cater for that case
+	// so I'm gonna pretend it's not possible.
+	fun intersection(other: Line): Point? {
+		if (this.simpleDirection == other.simpleDirection) {
+			return null
+		}
+
+		val (a, b) = run {
+			val x = this.normalize()
+			val y = other.normalize()
+
+			if ((x.simpleDirection) == SimpleDirection.VERTICAL) {
+				Pair(x, y)
+			} else {
+				Pair(y, x)
+			}
+		}
+
+		return if (
+			// Horizontal line starts before and ends after the vertical line.
+			a.a.x <= b.a.x && a.b.x >= b.a.x
+			// Horizontal line is in between the start and end y of the vertical line.
+			&& a.a.y >= b.a.y && a.a.y <= b.a.y
+		) {
+			Point(a.a.x, b.a.y)
+		} else {
+			null
+		}
+	}
+
+	/**
+	 * Creates a line where the direction is either UP or RIGHT.
+	 * This function will always create and return a new line.
+	 * For the returned Line, the following will hold true:
+	 * `a.x <= b.x && a.y <= b.y`
+	 */
+	private fun normalize(): Line {
+		return when (direction) {
+			Direction.UP, Direction.RIGHT -> Line(a.copy(), b.copy())
+			else -> Line(b.copy(), a.copy())
+		}
+	}
+}
+
+fun findNextCoordinate(original: Point, instruction: String): Point =
+	when (instruction[0]) {
+		// There HAS to be a better way to do this
+		'R' -> Point(original.x + instruction.substring(1).toInt(), original.y)
+		'U' -> Point(original.x, original.y + instruction.substring(1).toInt())
+		'L' -> Point(original.x - instruction.substring(1).toInt(), original.y)
+		'D' -> Point(original.x, original.y - instruction.substring(1).toInt())
+		else -> throw Exception("Unrecognized direction: ${instruction[0]}")
+	}
+
+fun main(args: Array<String>) {
+	if (args.size != 1) {
+		println("Usage: ./part-one <filename>")
+		return
+	}
+
+	val program = File(args[0])
+		.readLines()
+		.map { it.split(",") }
+	val intersections = mutableListOf<Point>()
+	var a = Point(0, 0)
+	for (aInstruction in program[0]) {
+		val aLine = Line(a, findNextCoordinate(a, aInstruction))
+		println("a line: $aLine")
+		var b = Point(0, 0)
+		for (bInstruction in program[1]) {
+			val bLine = Line(b, findNextCoordinate(b, bInstruction))
+			println("b line: $bLine")
+			val intersection = aLine.intersection(bLine)
+			if (intersection != null) {
+				intersections.add(intersection)
+			}
+			b = bLine.b
+		}
+		a = aLine.b
+	}
+	println(intersections)
+}
