564653242afb6195254bbf5f2047371ded7cf5a0
Juan de Bruin <juan@debruin.dev>
2021-12-08 22:19:24 +0200

    21-05(1): finished

---
+++ 2021/05/part1/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func main() {
+	vents := make(map[Point]int)
+	ReadInputFileByLine(func(line string) {
+		l := ParseLine(line)
+
+		if l.start.x == l.end.x {
+			// vertical line
+			var start int
+			var end int
+			if l.start.y < l.end.y {
+				start = l.start.y
+				end = l.end.y
+			} else {
+				start = l.end.y
+				end = l.start.y
+			}
+			for y := start; y <= end; y++ {
+				p := Point{l.start.x, y}
+
+				if v, ok := vents[p]; ok {
+					vents[p] = v + 1
+				} else {
+					vents[p] = 1
+				}
+			}
+		} else if l.start.y == l.end.y {
+			// horizontal line
+			var start int
+			var end int
+			if l.start.x < l.end.x {
+				start = l.start.x
+				end = l.end.x
+			} else {
+				start = l.end.x
+				end = l.start.x
+			}
+			for x := start; x <= end; x++ {
+				p := Point{x, l.start.y}
+
+				if v, ok := vents[p]; ok {
+					vents[p] = v + 1
+				} else {
+					vents[p] = 1
+				}
+			}
+		}
+	})
+
+	overlaps := 0
+	for _, v := range vents {
+		if v > 1 {
+			overlaps += 1
+		}
+	}
+	println(overlaps)
+}
+
+func ParseLine(text string) Line {
+	tokens := strings.Split(text, " ")
+	return Line{
+		start: ParsePoint(tokens[0]),
+		end:   ParsePoint(tokens[2]),
+	}
+}
+
+func ParsePoint(text string) Point {
+	tokens := strings.Split(text, ",")
+
+	x, err := strconv.Atoi(tokens[0])
+	if err != nil {
+		panic(err)
+	}
+
+	y, err := strconv.Atoi(tokens[1])
+	if err != nil {
+		panic(err)
+	}
+
+	return Point{x, y}
+}
+
+type Point struct {
+	x int
+	y int
+}
+
+type Line struct {
+	start Point
+	end   Point
+}
